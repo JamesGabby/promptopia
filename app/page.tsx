eb8ab8f2f41eb9da1e969cbbd3b3e036c6ad2a06
JamesGabby <jlghfe36@gmail.com>
2023-07-06 13:35:45 +0100

    create the necessary components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
+import Feed from "@/components/Feed"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+            <br className="max-hd:hidden" />
+            <span className="orange_gradient">AI-Powered Prompts</span>
+        </h1>
+        <p className="desc text-center">
+            Promptopia is an open-source AI prompting tool for the modern world to discoer, create and share creative prompts
+        </p>
+        <Feed />
+    </section>
   )
 }
 
