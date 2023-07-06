12713546c43daaf620ceb56a40cb0201d561c41a
JamesGabby <jlghfe36@gmail.com>
2023-07-06 01:54:16 +0100

    add static files, convert css to sass

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from 'react'
+import 'styles/globals.scss'
+
+export const metadata = {
+    title: 'Promptopia',
+    description: 'Discover and Share AI Prompts'
+}
+
+const RootLayout = ({ children }:PropsWithChildren) => {
+  return (
+    <html lang="en">
+        <body>
+            <div className="main">
+                <div className="gradient" />
+            </div>
+
+            <main className="app">
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
