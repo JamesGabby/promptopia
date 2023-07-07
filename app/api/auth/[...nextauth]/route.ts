98f7ae2b4a121aa3c5486d67896aef93aba209d1
JamesGabby <jlghfe36@gmail.com>
2023-07-07 17:15:23 +0100

    create user model

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { connectionToDB } from '@/utils/database'
 
 const handler = NextAuth({
     providers: [
@@ -12,7 +13,13 @@ const handler = NextAuth({
 
     },
     async signIn({ profile }) {
+        try {
+            await connectionToDB()
 
+            return true
+        } catch (error) {
+            console.log(error)
+        }
     }
 })
 
