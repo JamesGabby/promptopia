7c257141fa0941ca65f27c7655f27f8e1e2e5f8f
JamesGabby <jlghfe36@gmail.com>
2023-07-08 00:26:27 +0100

    fix routes and env file

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -22,7 +22,7 @@ const Nav = () => {
     return (
         <nav className="flex-between w-full mb-16 pt-3">
             <Link href="/" className="flex gap-2 flex-center">
-                <Image width={40} height={40} src={session?.user.image} alt="Promptopia Logo" className="object-contain" />
+                <Image width={40} height={40} src="/assets/images/logo.svg" alt="Promptopia Logo" className="object-contain" />
                 <p className="logo_text">Promptopia</p>
             </Link>
 
@@ -33,7 +33,7 @@ const Nav = () => {
                         <Link href="/create-prompt" className="black_btn">Create Post</Link>
                         <button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
                         <Link href="/profile">
-                            <Image className="rounded-full" src="/assets/images/logo.svg" width={37} height={37} alt="Profile" />
+                            <Image className="rounded-full" src={session?.user.image} width={37} height={37} alt="Profile" />
                         </Link>
                     </div>
                 ): (
@@ -51,7 +51,7 @@ const Nav = () => {
             <div className="sm:hidden flex relative">
                 {session?.user ? (
                     <div className="flex">
-                        <Image className="rounded-full" src="/assets/images/logo.svg" width={37} height={37} alt="Profile" onClick={() => setToggleDropdown((prev) => !prev)} />
+                        <Image className="rounded-full" src={session?.user.image} width={37} height={37} alt="Profile" onClick={() => setToggleDropdown((prev) => !prev)} />
                         {toggleDropdown && (
                             <div className="dropdown">
                                 <Link href="/profile" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
