40931166601c289c4a5189484d6e9f9daaa58397
JamesGabby <jlghfe36@gmail.com>
2023-07-27 19:50:29 +0100

    implement endpoint for update prompt

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,25 +1,25 @@
 import PromptCard from './PromptCard'
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
-  return (
-    <section className="w-full">
-      <h1 className='head_text text-left'>
-        <span className="blue_gradient">{name} profile</span>
-      </h1>
-      <p className="desc text-left">{desc}</p>
+    return (
+        <section className="w-full">
+            <h1 className='head_text text-left'>
+                <span className="blue_gradient">{name} profile</span>
+            </h1>
+            <p className="desc text-left">{desc}</p>
 
-      <div className="prompt_layout mt-10">
-        {data.map((prompt) => (
-          <PromptCard
-            key={prompt._id}
-            prompt={prompt}
-            handleEdit={() => handleEdit && handleEdit(prompt)}
-            handleDelete={() => handleDelete && handleDelete(prompt)} 
-          />
-        ))}
-      </div>
-    </section>
-  )
+            <div className="prompt_layout mt-10">
+                {data.map((prompt) => (
+                    <PromptCard
+                        key={prompt._id}
+                        prompt={prompt}
+                        handleEdit={() => handleEdit && handleEdit(prompt)}
+                        handleDelete={() => handleDelete && handleDelete(prompt)} 
+                    />
+                ))}
+            </div>
+        </section>
+    )
 }
 
 export default Profile
